938fef9b4f779d41f581fcfc987e245a965b29f6
newstudybd <164732490+newstudybd@users.noreply.github.com>
2024-03-24 10:45:43 +0000

    NEW

--- main/__init__.py
+++ main/__init__.py
@@ -14,8 +14,8 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 # variables
 API_ID = 22506688
 API_HASH = "e210ba8b245fffbaa7af50a901c6bee8"
-BOT_TOKEN = "6767554779:AAFD65XssBLPcQ3LIbh2zmXiceTWk0D3lLo"
-SESSION = "BQFXbMAAMc2Ce2PlL9RBJ7QSLJyLw6voM7pweQphsKqy2emmpq4ep0iJTZAVudZsEfQT6d1pnQolywLWRE4ssAsZAOOrMTsVlCe2XQzuWWqSPZZGDVWnBKKgERoIH07OP8yOfws4xTY3vUWUnO8vsyhdTASJ0QLgirXI5jUBdYH6Msvaw50feC8dJ1xnBZGWD3pw4zLsgRwBDl0hhOyNg113XVgQ_U9ADI_qcYCaBd_Gkuzt7cdZX6uH3sPEfnqnOcZK_Z6eoIvh3oKvAUQehT6hAEiTNndl8t24jpKY-zCsCoFMUGnenxODQRehcXknFCYZw2d-rLar8MYF1CVCCrGLMtMF9QAAAAGbj763AA-pVhKOSoZZ4v71wzmcVD5s2p5Fofb1MYi6IwmD5Vzqwmp9f6nrKTVYExXmHfzw-gonAbIIXtWNAK_j7JM9-pr82GHRVh4063sIdUklOJlteVJ0E87wDHcMJiQpcE0GjCw2QAUkI8qwp0pvtI3SAyBaEafaaHG-kENiVzIFE5NFVoOEAbLILzWVXNoBtlamz5xqkbsJmyqGx9PwN-wTH_m1vCcDtGldRFCjiIOuiXnhU0esJGuhK1w8In38bRYjeDgiiMDNkeeLQbI2UvTvstiENF-QXbdx2owSL_kbXf41NL7akHMwAAAAF-FBhiAA"
+BOT_TOKEN = "7057913309:AAFcSx8CR-EveyMXrXHoWOQh2EFBOPE0Qzg"
+SESSION = "BQFXbMAAMc2Ce2PlL9RBJ7QSLJyLw6voM7pweQphsKqy2emmpq4ep0iJTZAVudZsEfQT6d1pnQolywLWRE4ssAsZAOOrMTsVlCe2XQzuWWqSPZZGDVWnBKKgERoIH07OP8yOfws4xTY3vUWUnO8vsyhdTASJ0QLgirXI5jUBdYH6Msvaw50feC8dJ1xnBZGWD3pw4zLsgRwBDl0hhOyNg113XVgQ_U9ADI_qcYCaBd_Gkuzt7cdZX6uH3sPEfnqnOcZK_Z6eoIvh3oKvAUQehT6hAEiTNndl8t24jpKY-zCsCoFMUGnenxODQRehcXknFCYZw2d-rLar8MYF1CVCCrGLMtMF9QAAAAGbj763AA"
 AUTH = 6303728812
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
@@ -24,9 +24,9 @@ userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, ap
 
 try:
     userbot.start()
-# except BaseException:
-#     print("Userbot Error ! Have you added SESSION while deploying??")
-#     sys.exit(1)
+except BaseException:
+    print("Userbot Error ! Have you added SESSION while deploying??")
+    sys.exit(1)
 
 Bot = Client(
     "SaveRestricted",
